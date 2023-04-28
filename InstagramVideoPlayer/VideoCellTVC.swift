b3e666b5adc79c75795a38c25374379d34a2085d
Akshay Kumar <akshay.as597@gmail.com>
2023-04-28 23:06:22 +0530

    Video TableView Cell
    -> Adding TabeViewCell

---
+++ InstagramVideoPlayer/VideoCellTVC.swift
@@ -0,0 +1,26 @@
+//
+//  VideoCellTVC.swift
+//  InstagramVideoPlayer
+//
+//  Created by Akshay Kumar on 28/04/23.
+//
+
+import UIKit
+
+class VideoCellTVC: UITableViewCell {
+
+    //MARK: awakeFromNib
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    //MARK: SetSelected
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramVideoPlayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  InstagramVideoPlayer
+//
+//  Created by Akshay Kumar on 28/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Cell Outlets
+    @IBOutlet weak var videoTableView: UITableView!
+    
+    
+    //MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
